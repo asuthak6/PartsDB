3e49709c62f6cfe69c5d32b7558f0d0ced705a7c
asuthak6 <asuthak6@uwo.ca>
2024-10-29 11:18:29 -0400

    Get list of parts

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Parts store
+const parts = [
+    {id: 100, name: 'Belt', colour: 'brown'},
+    {id: 101, name: 'Clip', colour: 'brown'},
+    {id: 102, name: 'Belt', colour: 'red'},
+    {id: 103, name: 'Hat', colour: 'purple'}
+];
+
+// Get list of parts
+app.get('/api/parts', (req, res) => {
+    console.log(`GET request for ${req.url}`);
+    res.send(parts);
 });
 
 app.listen(port, () => {
